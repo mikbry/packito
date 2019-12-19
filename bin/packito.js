1895189d33802d5dd22e9280f77b1f11950bdb04
Mik <mik@opla.ai>
2019-12-19 11:48:04 +0100

    fix: dev packito used twice

--- bin/packito.js
+++ bin/packito.js
@@ -13,32 +13,19 @@
 // chmod +x packito.js
 
 (async () => {
-  let d;
-  let cli;
   const output = console;
   if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
     try {
-      d = await import('../src/cli');
-      cli = d.default;
-
-      if (cli) {
-        await cli();
-      } else {
-        output.error(`Packito can't be started`);
-      }
+      await import('../src/cli');
     } catch (e) {
       //
     }
   } else {
     try {
       const path = require('path');
-      const [, , ...args] = process.argv;
-      console.log('path=', args[0]);
-      const p = args[0] || './index.js';
       // eslint-disable-next-line import/no-unresolved
       // eslint-disable-next-line import/no-dynamic-require
-      d = require(path.join(__dirname, p));
-      cli = d.default;
+      require(path.join(__dirname, '../index.js'));
     } catch (e) {
       //
       output.error(e);
